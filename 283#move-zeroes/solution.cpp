28f66a1d866ddb427935ef8f767762d700201ec8
Wei Li <llwwns@gmail.com>
2016-05-19 11:46:17 +0900

    Fix 283

--- 283#move-zeroes/solution.cpp
+++ 283#move-zeroes/solution.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i, j = 0, len = nums.size();
-        for (i = 0; i < len; i++) {
-            if (nums[i] != 0) nums[j++] = nums[i];
+        auto p = nums.begin();
+        auto q = nums.begin();
+        auto e = nums.end();
+        for (;p != e; p++) {
+            if (*p  != 0) {
+                *(q++) = *p;
+            }
         }
-        while (j < i) {
-            nums[j++] = 0;
+        while (q != p) {
+            *(q++) = 0;
         }
     }
 };
